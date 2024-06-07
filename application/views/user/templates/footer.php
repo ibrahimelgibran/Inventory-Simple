729ad6be74482af93149da3c059f597368db6dd7
ElGibran17 <ibrahimelgibran17@gmail.com>
2024-06-07 11:30:55 +0700

    first commit

---
+++ application/views/user/templates/footer.php
@@ -0,0 +1,7 @@
+  <footer class="cp-footer">
+    <div class="container">
+        <h3 class="title-footer">Copyright &copy; <?=date('Y')?> By IEGCODE</h3>
+    </div>
+  </footer>
+  </body>
+</html>
